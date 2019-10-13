8c98908bd4063d49be94ff06fdf91f4fcdb3bb84
Rachael Homann <rachaelhomann@gmail.com>
2019-10-13 10:45:58 -0500

    div elements hidden / shown upon start click

--- app.js
+++ app.js
@@ -5,68 +5,91 @@ var time = questions.length * 15;
 var questionDiv = document.getElementById('question');
 var choicesDiv = document.getElementById('choices');
 var questionBtn = document.getElementById('questionBtn');
-var startQuestion = 0; 
+var quizBlock = document.getElementById('quizQuestions');
 
 //questions time choices submit start initials
 //TIMER
 
 var startBtn = document.querySelector('#startBtn');
-var timeElement = document.querySelector('.timerElement');
+var timeElement = document.querySelector('#time');
 var secondsLeft = 75;
+var timerInterval;
 
-function startScreen() {
-  document.getElementById("quizQuestions").style.display = "none";
-};
+quizQuestions.style.display = "none";
+
+startBtn.onclick = function() { 
+  quizQuestions.style.display = "block";
+  startBtn.style.display = "none"; 
+} 
 
 startBtn.addEventListener('click', function() {
     setTime();
 });
 
 function setTime() {
-    var timerInterval = setInterval(function() {
+  timerInterval = setInterval(function() {
     secondsLeft--;
-    timeElement.textContent = secondsLeft;
 
-    if(secondsLeft === 0) {
+    if(secondsLeft <= 0) {
       clearInterval(timerInterval);
-      sendMessage();
+      secondsLeft = 0; // "-5" wouldn't make sense to show
+     // sendMessage();
     }
+
+    timeElement.textContent = secondsLeft;
 
   }, 1000);
 };
 
-function sendMessage() {
+/*function sendMessage() {
   timeElement.textContent = "Please enter your initials:";
 
   var input = document.createElement('input'); 
     input.type = "text"; 
     container.appendChild(input); 
 
-};
+};*/
 
 startBtn.addEventListener('click', function() {
-  startQuestion++;
-  /* call redner question */
-  questionDiv.innerHTML = renderQuestion(startQuestion);
-  renderChoices(startQuestion);
-  
+  /* call render question */
+  questionDiv.innerHTML = renderQuestion(currentQuestionIndex);
+  renderChoices(currentQuestionIndex);
+    
 });
-
-
- //array of questions (question is string, choices are array, answer is string, key is number (to id into local storage)) (for loop through choices, chekc pbject for answer, if matches mark as correct)
-    //score
 
   /* helpers */
 /* takes in index, uses that index to find question in db to return the title */
+questionDiv.innerHTML = renderQuestion(currentQuestionIndex);
+renderChoices(currentQuestionIndex);
+
 function renderQuestion(index) {
     return questions[index].title;
 };
+
+function renderNextQuestion(questionIndex) {
+  questionDiv.innerHTML = renderQuestion(questionIndex);
+  renderChoices(questionIndex);
+}
 
 function checkAnswer(event) {
   var buttonEl = event.target;
   console.log(buttonEl.textContent);
   console.log(buttonEl.questionIndex);
   console.log(buttonEl.textContent === questions[buttonEl.questionIndex].answer);
+
+  if(buttonEl.textContent === questions[buttonEl.questionIndex].answer) {
+    if(currentQuestionIndex < questions.length - 1) {
+      currentQuestionIndex++;
+      renderNextQuestion(currentQuestionIndex);
+    } else {
+      // end quiz
+      var finalScore = secondsLeft;
+      clearInterval(timerInterval);
+      console.log('finalScore is:', secondsLeft);
+    }
+  } else {
+    secondsLeft -= 15;
+  }
 }
 
 /* takes in index, uses this index to return the choices from the db */
@@ -76,32 +99,9 @@ function renderChoices(index) {
         var choiceBtn = document.createElement('button');
         choiceBtn.className = 'btn btn-primary';
         choiceBtn.textContent = questions[index].choices[i];
-        choiceBtn.questionIndex = index; //[i]?
+        choiceBtn.questionIndex = index;
         choiceBtn.onclick = checkAnswer;
         choicesDiv.appendChild(choiceBtn);
 
-        // if button click id === choiceBtn.id, set var currentQuestion = that question
-        // if user answer === currentQuestion.answer, return true
-        // else subtract 15s 
-
-        //if the button click id === choiceBtn.id, return true
-        //if the button click id !=== choiceBtn.id, return false and subtract 15 seconds from time
-
     };
 };
-
-/* events */
-//init
-
-questionDiv.innerHTML = renderQuestion(startQuestion);
-  renderChoices(0);
-
-
-//function getQuestion() {
-  //var currentQuestion = questions[currentQuestionIndex];
- 
-//};
-
-//function questionClick() {
-//  if 
-//};
